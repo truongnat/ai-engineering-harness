8cbc6ad7b565ecfe384393ffdfe25535d5e4a14b
truongdq <truongdq.dev@gmail.com>
2026-06-03 16:21:00 +0700

    feat(site): compose full landing page

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,3 +1,31 @@
+import { AnimatedGrid } from './components/AnimatedGrid'
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import { ProblemSection } from './components/ProblemSection'
+import { CommandFlow } from './components/CommandFlow'
+import { ArtifactShowcase } from './components/ArtifactShowcase'
+import { DemoTerminal } from './components/DemoTerminal'
+import { ProviderCards } from './components/ProviderCards'
+import { FeatureGrid } from './components/FeatureGrid'
+import { InstallSection } from './components/InstallSection'
+import { CTA } from './components/CTA'
+
 export default function App() {
-  return <div />
+  return (
+    <>
+      <AnimatedGrid />
+      <Navbar />
+      <main>
+        <Hero />
+        <ProblemSection />
+        <CommandFlow />
+        <ArtifactShowcase />
+        <DemoTerminal />
+        <ProviderCards />
+        <FeatureGrid />
+        <InstallSection />
+        <CTA />
+      </main>
+    </>
+  )
 }
